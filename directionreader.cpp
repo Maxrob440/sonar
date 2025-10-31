223bdd49c04ea98c6a6fab2ebbd39cd545a0bb45
Max_Robertson <max.robertson440@icloud.com>
2025-10-31 20:35:37 +0000

    comments and shorter wait

--- directionreader.cpp
+++ directionreader.cpp
@@ -4,40 +4,44 @@
 #include <chrono>
 #include <thread>
 #include <iostream>
+
+
 DirectionReader::DirectionReader(int handle):h(handle) {
   lgGpioClaimOutput(h,0,PIN,0);
 }
 
 double DirectionReader::getDirection(){
+    // last orientation of the servo in degrees
     return direction;
 }
 
 double DirectionReader::getRadiansDirection(){
+    //Last orientation of the servo in radians
     return direction*(3.1415926535897932/180);
 }
 
 int DirectionReader::angleToPulse(double angleDeg,
                    int minUs=1000,
                    int maxUs=2000){
-    if (angleDeg<-90) angleDeg=-90;
-    if (angleDeg>90) angleDeg=90;
-    double t = (angleDeg +90.0)/180.0;
-    return static_cast<int>(minUs+t*(maxUs-minUs));
+    if (angleDeg<-90) angleDeg=-90; //stops from hitting end
+    if (angleDeg>90) angleDeg=90; // stops hitting the end the other way
+
+    double t = (angleDeg +90.0)/180.0; // normalises an angle to between 0-1
+    return static_cast<int>(minUs+t*(maxUs-minUs)); // converts to int and returns as a value between minUs and maxUs
   }
+
 void DirectionReader::setDirection(double angleDeg){
+  // Code to move the servo
+  // PIN is gpio 17 which is pin 11 on the pi
   lgTxServo(h,PIN,angleToPulse(angleDeg),FREQ,0,0);
 }
+
 void DirectionReader::moveDirection(double angleDeg){
-  if (direction>89 and directionMove == 1 ){
-    directionMove = -1;
-  }
-  else if (direction < -89 and directionMove == -1){
-    directionMove =1;
-  }
+  // adjusts the current angle by the angleDeg
+  if (direction>89 and directionMove==1){directionMove=-1;} // if moving up and angle is 90* move back down
+  else if (direction <-89 and directionMove==-1){directionMove =1;} // if moving down and angle is -90 start moving up
     direction=direction + angleDeg*directionMove;
     setDirection(direction);
-    std::cout<<angleDeg <<"angle\n";
-    std::cout<<direction<<"\n";
-  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
